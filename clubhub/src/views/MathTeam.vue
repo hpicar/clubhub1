24386c6849205b54d5e9d1b725f04a4a81ed7170
destinyshishido <itsmedest2003@gmail.com>
2020-10-10 12:13:00 -1000

    left bar for forms added to nths, model un, and math team
    yee

--- clubhub/src/views/MathTeam.vue
+++ clubhub/src/views/MathTeam.vue
@@ -15,12 +15,43 @@
                 <span>About</span>
             </v-btn>
         </v-toolbar>
+
+        <v-navigation-drawer
+            floating
+            permanent
+            class = "pt-15"
+        >
+            <v-list
+            dense
+            rounded
+            >
+            <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                link
+            >
+            <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
     </div>
 </template>
 
 <script>
 export default {
-    components: {
-    }
-}
+    data () {
+      return {
+        items: [
+          { title: 'Membership Forms', icon: 'mdi-view-dashboard' },
+          { title: 'Field Trip Forms', icon: 'mdi-forum' },
+        ],
+      }
+    },
+  }
 </script>
--- clubhub/src/views/ModelUN.vue
+++ clubhub/src/views/ModelUN.vue
@@ -3,20 +3,53 @@
         <v-toolbar class="mt-1" height="50">
             <v-toolbar-title class="ml-15">Model UN</v-toolbar-title>
             <v-spacer></v-spacer>
+
             <v-btn text>
                 <v-icon>mdi-calendar-blank</v-icon>
                 <span>Events</span>
             </v-btn>
+
             <v-btn text>
                 <span>About</span>
             </v-btn>
         </v-toolbar>
+
+        <v-navigation-drawer
+            floating
+            permanent
+            class = "pt-15"
+        >
+            <v-list
+            dense
+            rounded
+            >
+            <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                link
+            >
+            <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
     </div>
 </template>
 
 <script>
 export default {
-    components: {
-    }
-}
+    data () {
+      return {
+        items: [
+          { title: 'Membership Forms', icon: 'mdi-view-dashboard' },
+          { title: 'Field Trip Forms', icon: 'mdi-forum' },
+        ],
+      }
+    },
+  }
 </script>
