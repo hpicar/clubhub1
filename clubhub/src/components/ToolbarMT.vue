c20d92a5b3865172c0bbb6a71bb9b93e4c736f0e
reyanlee <anni3lee03@gmail.com>
2020-10-15 08:07:40 -1000

    created ToolbarMT.vue component

---
+++ clubhub/src/components/ToolbarMT.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-toolbar class="mt-1" height="50">
+      <v-toolbar-title class="ml-15">Math Team</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text v-for="link in links" :key="link.name" :to="link.route">
+        <v-icon left>{{ link.icon }}</v-icon>
+        <span>{{ link.name }}</span>
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyClubs',
+  data() {
+    return {
+      links: [
+        { name: 'Calendar', icon: 'mdi-calendar-blank', route: '/mathteamcalendar' },
+        { name: 'Updates', icon: 'mdi-home', route: '/mathteam' },
+        { name: 'Resources', icon: 'mdi-file-document-multiple', route: '/mathteamresources' },
+        { name: 'About', icon: 'mdi-information', route: '/mathteamabout' }
+      ]
+    }
+  }
+}
+</script>
