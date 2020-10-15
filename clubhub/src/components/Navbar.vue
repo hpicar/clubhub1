50b85dd316746ad6e17aa88a83da805559c02e51
reyanlee <anni3lee03@gmail.com>
2020-10-14 22:24:29 -1000

    Update Navbar.vue
    deleted nav drawer, added my/browse clubs

--- clubhub/src/components/Navbar.vue
+++ clubhub/src/components/Navbar.vue
@@ -1,6 +1,5 @@
 <template>
     <v-app-bar app height="100" color="blue darken-3" dark>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
         <v-toolbar-title>
             <h1>ClubHub</h1>
@@ -8,30 +7,10 @@
     
         <v-spacer></v-spacer>
 
-        <v-btn text>
-            <span class="mr-2">Master Calendar</span>
+        <v-btn text v-for="link in links" :key="link.text" :to="link.route">
+            <v-icon left>{{ link.icon }}</v-icon>
+            <span class="mr-2">{{ link.text }}</span>
         </v-btn>
-
-        <v-btn text>
-            <span class="mr-2">About</span> 
-        </v-btn>
-
-        <v-btn text>
-            <v-icon left>mdi-account</v-icon>
-            <span class="mr-2">My Account</span> 
-        </v-btn>
-
-        <v-navigation-drawer app absolute v-model="drawer" color="blue darken-1">
-            <v-list>
-                <v-list-item>
-                    <v-list-title class="title">Waipahu High School</v-list-title>
-                </v-list-item>
-                                
-                <v-list-item v-for="link in links" :key="link.text" :to="link.route">
-                        <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
     
     </v-app-bar>
 </template>
@@ -40,10 +19,11 @@
 export default {
     data() {
         return {
-            drawer: false,
             links: [
-                { text: 'My Clubs', route: '/' },
-                { text: 'Browse Clubs', route: '/browse' },
+                { text: 'Master Calendar', icon:'mdi-calendar', route: '' },
+                { text: 'My Clubs', icon:'mdi-home', route: '/' },
+                { text: 'Browse Clubs', icon:'mdi-magnify', route: '/browse' },
+                { text: 'My Account', icon:'mdi-account', route: '/' },
             ],
         }
     }
